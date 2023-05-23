067bebf23555407ea8ead2403142b3f19b427dd2
Vinao <vinicius250703@gmail.com>
2023-05-23 17:54:39 -0300

    Código pronto

--- resto.py
+++ resto.py
@@ -1,11 +1,10 @@
 vl_minimo = int(input("Digite o valor minimo esperado: "))
-
-lista_num = int(input("Digite um valor: "))
 soma = 0
  
 while soma < vl_minimo :
-    soma = lista_num
     lista_num = int(input("Digite outro valor: "))
+    soma += lista_num
+    
     
 
 print('{}' .format(vl_minimo))
