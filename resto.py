6a324de4d02f55465b0cf6afd8ae045237211f3c
Vinao <vinicius250703@gmail.com>
2023-05-23 17:26:43 -0300

    add new cod

---
+++ resto.py
@@ -0,0 +1,13 @@
+vl_minimo = int(input("Digite o valor minimo esperado: "))
+
+lista_num = int(input("Digite um valor: "))
+soma = 0
+ 
+while soma < vl_minimo :
+    soma = lista_num
+    lista_num = int(input("Digite outro valor: "))
+    
+
+print('{}' .format(vl_minimo))
+print('{}' .format(soma))
+print('{}' .format(soma-vl_minimo))
